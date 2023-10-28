53f332579753a4bb042a85ba5c5f4161ac6cb8e6
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-28 18:45:13 +0330

    add colors to tailwind confog

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          10: "#DABB9E",
+          90: "#533529",
+        },
+      },
+    },
   },
   plugins: [],
 };
