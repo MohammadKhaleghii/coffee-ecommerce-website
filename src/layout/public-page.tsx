a9e864df54bb1e74fb120bf919aa4c430f049c8b
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-28 13:55:33 +0330

    add header item to pulice page layout

--- src/layout/public-page.tsx
+++ src/layout/public-page.tsx
@@ -1,11 +1,63 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function PublicPageLayout({ children }: { children: React.ReactNode }) {
+export default function PageLayout({ children }: { children: React.ReactNode }) {
+
+    const navigationItem = [
+        {
+            title: "صفحه اصلی",
+            href: "/",
+        },
+        {
+            title: "فروشگاه",
+            href: "/shop",
+        },
+        {
+            title: "پشتیبانی",
+            href: "/suppout",
+        },
+        {
+            title: "درباره ما",
+            href: "/about",
+        },
+        {
+            title: "تماس ما",
+            href: "/contact",
+        },
+
+
+    ]
+    const navigationIcons = [
+        {
+            iconClassName: "fa-solid fa-user",
+            href: "#"
+        },
+        {
+            iconClassName: "fa-solid fa-cart-shopping",
+            href: "#"
+        },
+        {
+            iconClassName: "fas fa-search",
+            href: "#"
+        },
+
+    ]
     return (
-        <>
-            <header>
+        <div className='max-w-screen-2xl min-w-[360px] my-0 mx-auto'>
+            <header className='px-32 pt-8 bg-red-50'>
+                <nav>
+                    {navigationItem.map((navItem, index) => (
+                        <ul key={index}>
+                            <Link to={navItem.href} ><li>{navItem.title}</li></Link>
+                        </ul>
+                    ))}
+                </nav>
                 <div>
-                    
+                    {navigationIcons.map((navItem, index) => (
+                        <ul key={index}>
+                            <Link to={navItem.href} ><i className={`${navItem.iconClassName}`} /></Link>
+                        </ul>
+                    ))}
                 </div>
             </header>
             <main>
@@ -14,6 +66,6 @@ export default function PublicPageLayout({ children }: { children: React.ReactNo
             <footer>
 
             </footer>
-        </>
+        </div>
     )
 }
