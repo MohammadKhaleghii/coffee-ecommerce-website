c428f4ef0e98f116db268d07ac0cd6b9ae6a3cac
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-28 13:53:56 +0330

    add home route to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/home/index';
 
 function App() {
   return (
- <>
- 
- </>
+    <Routes>
+      <Route Component={Home} path="/" />
+    </Routes>
   );
 }
 
