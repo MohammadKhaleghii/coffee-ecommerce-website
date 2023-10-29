78b64a2c10f631b3cb88dd6e1fa56a173312b102
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-29 21:00:49 +0330

    add support route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/home/index';
 import Shop from "./pages/shop";
+import Suppout from "./pages/suppout";
 
 function App() {
   return (
     <Routes>
       <Route Component={Home} path="/" index />
       <Route Component={Shop} path="/shop" />
+      <Route Component={Suppout} path="/suppout" />
     </Routes>
   );
 }
