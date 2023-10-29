ad47933841ad2889ad32c359ed69616e9a90bdd9
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-29 21:03:08 +0330

    add about page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,9 @@
+import PageLayout from "./../../layout/public-page";
+
+export default function About() {
+  return (
+    <PageLayout>
+      <section className="px-2 pb-10"></section>
+    </PageLayout>
+  );
+}
