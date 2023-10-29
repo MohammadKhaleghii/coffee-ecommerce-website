c9a24215fd22b378f08d7d4079c952a25102e93f
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-29 21:03:03 +0330

    add about route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/home/index';
 import Shop from "./pages/shop";
 import Suppout from "./pages/suppout";
 import Contact from "./pages/contact";
+import About from "./pages/about";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route Component={Shop} path="/shop" />
       <Route Component={Suppout} path="/suppout" />
       <Route Component={Contact} path="/contact" />
+      <Route Component={About} path="/about" />
     </Routes>
   );
 }
