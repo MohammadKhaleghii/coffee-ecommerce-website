f3e2ff027f0a158659bbd50a95f0f989af0a60f8
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-29 20:57:49 +0330

    add shop route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/home/index';
+import Shop from "./pages/shop";
 
 function App() {
   return (
     <Routes>
-      <Route Component={Home} path="/" />
+      <Route Component={Home} path="/" index />
+      <Route Component={Shop} path="/shop" />
     </Routes>
   );
 }
