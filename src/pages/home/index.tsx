975fcce9674afa17792291905da29ec03621acb6
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-29 11:53:57 +0330

    add category list

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,38 @@
-import PageLayout from './../../layout/public-page';
+import CategoryList from "../../container/category-list/category-list";
+import PageLayout from "./../../layout/public-page";
 
 export default function Home() {
-    return (
-        <PageLayout>
-
-        </PageLayout>
-    )
+  const categoryList = [
+    {
+      categoryTitle: "عنوان تستی",
+      categoryImage: "/assetes/image/home-assetes/1.png",
+    },
+    {
+      categoryTitle: "عنوان تستی",
+      categoryImage: "/assetes/image/home-assetes/1.png",
+    },
+    {
+      categoryTitle: "عنوان تستی",
+      categoryImage: "/assetes/image/home-assetes/1.png",
+    },
+    {
+      categoryTitle: "عنوان تستی",
+      categoryImage: "/assetes/image/home-assetes/1.png",
+    },
+    {
+      categoryTitle: "عنوان تستی",
+      categoryImage: "/assetes/image/home-assetes/1.png",
+    }
+  
+  ];
+  return (
+    <PageLayout>
+      <section>
+        <CategoryList title={"دسته بندی محصولات"} categoryList={categoryList} />
+      </section>
+      <section className="pt-[92px]">
+    
+      </section>
+    </PageLayout>
+  );
 }
