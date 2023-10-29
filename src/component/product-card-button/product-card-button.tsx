2b194b40bc36f2ca9219a1f63dcbec2f3c909406
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-29 21:26:36 +0330

    add button to product cart button

--- src/component/product-card-button/product-card-button.tsx
+++ src/component/product-card-button/product-card-button.tsx
@@ -1,9 +1,12 @@
 import { ProductCardButtonProps } from "./product-card-button.interface";
 
 const ProductCartButton: React.FC<ProductCardButtonProps> = (props: ProductCardButtonProps)=>{
-    return(
-        <>
-        </>
-    )
+    return (
+        <div className="flex flex-row gap-x-2 justify-center items-center pb-2">
+          <button className="rounded-full bg-[#F9C06A] px-4 py-2 w-fit border-none">
+            افزودن به سبد خرید
+          </button>
+        </div>
+    );
 };
 export default ProductCartButton;
