723c23fb25b55cb8df62ed6f0a88adb5995845a7
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-29 21:01:00 +0330

    add support page

---
+++ src/pages/suppout/index.tsx
@@ -0,0 +1,9 @@
+import PageLayout from "./../../layout/public-page";
+
+export default function Suppout() {
+  return (
+    <PageLayout>
+      <section className="px-2 pb-10"></section>
+    </PageLayout>
+  );
+}
