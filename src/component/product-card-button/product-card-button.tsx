ca3274b9683f1ee016636ebb76340fef0d922456
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-29 21:25:39 +0330

    add product cart button

---
+++ src/component/product-card-button/product-card-button.tsx
@@ -0,0 +1,9 @@
+import { ProductCardButtonProps } from "./product-card-button.interface";
+
+const ProductCartButton: React.FC<ProductCardButtonProps> = (props: ProductCardButtonProps)=>{
+    return(
+        <>
+        </>
+    )
+};
+export default ProductCartButton;
