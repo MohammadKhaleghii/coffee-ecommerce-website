78749b635251e86f465d4262f70773759aeb0784
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-31 12:16:33 +0330

    update reducers for config store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./reducre/cart.reducer";
+import { combineReducers } from "redux";
 
-
+const rootReducer = combineReducers({
+  [cartSlice.name]: cartSlice.reducer,
+});
 export const store = configureStore({
-    reducer: cartSlice.reducer
-})
+  reducer: rootReducer,
+});
