96e2fabce7b4b10e5987ea9d1ea08bee912a52dc
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-10-31 08:16:56 +0330

    add cart page

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -3,7 +3,179 @@ import PageLayout from "./../../layout/public-page";
 export default function Cart() {
   return (
     <PageLayout>
-      <section className="px-2 pb-10"></section>
+      <section className="px-2 pb-10">
+        <div className="container mx-auto mt-10">
+          <div className="flex shadow-md my-10">
+            <div className="w-full bg-white px-10 py-10">
+              <div className="flex justify-between border-b pb-8">
+                <h1 className="font-semibold text-2xl">سبد خرید</h1>
+                <h2 className="font-semibold text-2xl"></h2>
+              </div>
+              <div className="flex mt-10 mb-5">
+                <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">
+                  جزئیات محصول
+                </h3>
+                <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
+                  تعداد
+                </h3>
+                <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
+                  قیمت
+                </h3>
+                <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
+                  جمع
+                </h3>
+              </div>
+              <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+                <div className="flex w-2/5">
+                  <div className="w-20">
+                    <img
+                      className="h-24"
+                      src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
+                      alt=""
+                    />
+                  </div>
+                  <div className="flex flex-col justify-between ml-4 flex-grow">
+                    <span className="font-bold text-sm">Iphone 6S</span>
+                    <span className="text-red-500 text-xs">Apple</span>
+                    <a
+                      href="#"
+                      className="font-semibold hover:text-red-500 text-gray-500 text-xs"
+                    >
+                      Remove
+                    </a>
+                  </div>
+                </div>
+                <div className="flex justify-center w-1/5">
+                  <svg
+                    className="fill-current text-gray-600 w-3"
+                    viewBox="0 0 448 512"
+                  >
+                    <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                  </svg>
+
+                  <input
+                    className="mx-2 border text-center w-8"
+                    type="text"
+                    value="1"
+                  />
+
+                  <svg
+                    className="fill-current text-gray-600 w-3"
+                    viewBox="0 0 448 512"
+                  >
+                    <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                  </svg>
+                </div>
+                <span className="text-center w-1/5 font-semibold text-sm">
+                  $400.00
+                </span>
+                <span className="text-center w-1/5 font-semibold text-sm">
+                  $400.00
+                </span>
+              </div>
+
+              <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+                <div className="flex w-2/5">
+                  <div className="w-20">
+                    <img
+                      className="h-24"
+                      src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
+                      alt=""
+                    />
+                  </div>
+                  <div className="flex flex-col justify-between ml-4 flex-grow">
+                    <span className="font-bold text-sm">
+                      Xiaomi Mi 20000mAh
+                    </span>
+                    <span className="text-red-500 text-xs">Xiaomi</span>
+                    <a
+                      href="#"
+                      className="font-semibold hover:text-red-500 text-gray-500 text-xs"
+                    >
+                      Remove
+                    </a>
+                  </div>
+                </div>
+                <div className="flex justify-center w-1/5">
+                  <svg
+                    className="fill-current text-gray-600 w-3"
+                    viewBox="0 0 448 512"
+                  >
+                    <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                  </svg>
+
+                  <input
+                    className="mx-2 border text-center w-8"
+                    type="text"
+                    value="1"
+                  />
+
+                  <svg
+                    className="fill-current text-gray-600 w-3"
+                    viewBox="0 0 448 512"
+                  >
+                    <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                  </svg>
+                </div>
+                <span className="text-center w-1/5 font-semibold text-sm">
+                  $40.00
+                </span>
+                <span className="text-center w-1/5 font-semibold text-sm">
+                  $40.00
+                </span>
+              </div>
+
+              <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+                <div className="flex w-2/5">
+                  <div className="w-20">
+                    <img
+                      className="h-24"
+                      src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
+                      alt=""
+                    />
+                  </div>
+                  <div className="flex flex-col justify-between ml-4 flex-grow">
+                    <span className="font-bold text-sm">Airpods</span>
+                    <span className="text-red-500 text-xs">Apple</span>
+                    <a
+                      href="#"
+                      className="font-semibold hover:text-red-500 text-gray-500 text-xs"
+                    >
+                      Remove
+                    </a>
+                  </div>
+                </div>
+                <div className="flex justify-center w-1/5">
+                  <svg
+                    className="fill-current text-gray-600 w-3"
+                    viewBox="0 0 448 512"
+                  >
+                    <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                  </svg>
+                  <input
+                    className="mx-2 border text-center w-8"
+                    type="text"
+                    value="1"
+                  />
+
+                  <svg
+                    className="fill-current text-gray-600 w-3"
+                    viewBox="0 0 448 512"
+                  >
+                    <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                  </svg>
+                </div>
+                <span className="text-center w-1/5 font-semibold text-sm">
+                  $150.00
+                </span>
+                <span className="text-center w-1/5 font-semibold text-sm">
+                  $150.00
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </PageLayout>
   );
 }
