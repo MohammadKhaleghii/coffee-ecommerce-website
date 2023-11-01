a253f7a31e3bf01b6ac629f070b1a55e323abe67
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-01 11:36:07 +0330

    rename db to firestore config

---
+++ src/services/firestore-config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth , signInWithPopup , signInWithRedirect , GoogleAuthProvider } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBySnltyI6tChuFktBkvFPPrn--TqK2sjw",
+  authDomain: "coffe-web-bc1c8.firebaseapp.com",
+  projectId: "coffe-web-bc1c8",
+  storageBucket: "coffe-web-bc1c8.appspot.com",
+  messagingSenderId: "303271874741",
+  appId: "1:303271874741:web:4f91b342128bab7f37aefc",
+  measurementId: "G-C4JPJDW2HQ",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
