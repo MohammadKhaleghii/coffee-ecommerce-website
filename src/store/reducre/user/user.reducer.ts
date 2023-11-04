f347c49dda8ecb2b8d7d1cf8b07f93357c92334c
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-04 11:41:17 +0330

    add user reducer

--- src/store/reducre/user/user.reducer.ts
+++ src/store/reducre/user/user.reducer.ts
@@ -1,13 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { UserSliceTypes } from "./user.types";
 
-const initialState: UserSliceTypes | null = {
-  displayName: "",
-  email: "",
-};
+const initialState: UserSliceTypes | null = null;
 
 export const userSlice = createSlice({
-  name: "userReducer",
+  name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    removeCurrnetUser(state) {
+      state = null;
+    },
+    currentUser(state, action) {
+      return (state = action.payload);
+    },
+  },
 });
+
+export const { removeCurrnetUser, currentUser } = userSlice.actions;
