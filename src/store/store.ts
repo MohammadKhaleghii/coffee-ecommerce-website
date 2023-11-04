9db4d5030194ebf5e8fb7aa355c2ac9af3972703
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-04 10:55:32 +0330

    add userslice to cart

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./reducre/cart/cart.reducer";
 import { combineReducers } from "redux";
+import { userSlice } from "./reducre/user/user.reducer";
 
 const rootReducer = combineReducers({
   [cartSlice.name]: cartSlice.reducer,
+  [userSlice.name]: cartSlice.reducer,
 });
 export const store = configureStore({
   reducer: rootReducer,
