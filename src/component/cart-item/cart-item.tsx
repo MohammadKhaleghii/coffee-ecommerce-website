1a4f33aca84924f4b0d64f35cfa0ba821726500f
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-06 14:32:20 +0330

    add height to cart item

--- src/component/cart-item/cart-item.tsx
+++ src/component/cart-item/cart-item.tsx
@@ -37,8 +37,8 @@ const CartItem: React.FC<CartItemProps> = ({
   return (
     <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
       <div className="flex w-2/5">
-        <div className="w-20">
-          <img className="h-24" src={imagePath} alt="" />
+        <div className="w-24 mx-2">
+          <img className="h-24  w-24" src={imagePath} alt="" />
         </div>
         <div className="flex flex-col justify-between ml-4 flex-grow">
           <span className="font-bold text-sm">{productTitle}</span>
