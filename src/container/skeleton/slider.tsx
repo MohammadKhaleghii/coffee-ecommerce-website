0dfa78898dbcc7438a117b1cbf95b3a75be3cb68
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-06 11:03:09 +0330

    add slider skeleton

---
+++ src/container/skeleton/slider.tsx
@@ -0,0 +1,9 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const SliderSkeleton = () => {
+  return (
+      <Skeleton className="rounded-2xl lg:h-[300px] h-[60px]"   />
+  );
+};
+export default SliderSkeleton;
