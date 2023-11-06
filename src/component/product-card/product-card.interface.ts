fb2790069b3a6cdd4ff6ab61765a8a23b0bc8939
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-06 10:23:51 +0330

    update product card props

--- src/component/product-card/product-card.interface.ts
+++ src/component/product-card/product-card.interface.ts
@@ -2,8 +2,10 @@ export interface ProductCardProps {
   ProductDetails: ProductDetails;
 }
 export interface ProductDetails {
+  imagePath: string;
   productTitle: string;
-  productImage: string;
+  productBrand: string;
+  productID: number;
   productPrice: number;
   productCombination: ProductCombination;
 }
