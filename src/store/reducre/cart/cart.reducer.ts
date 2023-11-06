9788a5571f8fcc8aa89a0af9165810afad044bdd
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-06 12:14:57 +0330

    add add to cart reducer

--- src/store/reducre/cart/cart.reducer.ts
+++ src/store/reducre/cart/cart.reducer.ts
@@ -7,10 +7,32 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart: (state, action) => {},
+    addToCart: (state, action) => {
+      const findProduct = state.find(
+        (item) => item.productID === action.payload.productID
+      );
+      if (findProduct) {
+        const increaseQTY = {
+          ...action.payload,
+          productQTY: action.payload.productQTY + 1,
+        };
+        return state.map((product) => {
+          return findProduct.productID === action.payload.productID
+            ? {
+                ...product,
+                productQTY: product.productQTY + 1,
+              }
+            : product;
+        });
+      } else {
+        state.push({ ...action.payload, productQTY: 1 });
+      }
+    },
     decreaseItemInTocart: (state, action) => {},
     removeFromCart: (state, action) => {},
   },
 });
 
-export default cartSlice.reducer;
+export default cartSlice;
+export const { addToCart, decreaseItemInTocart, removeFromCart } =
+  cartSlice.actions;
