46c576bdc4cb33e5bf0d0fd47795f8611bc4f48a
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-06 10:40:36 +0330

    delete muck data

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,180 +1,40 @@
+import { DocumentData } from "firebase/firestore";
+import { ProductDetails } from "../../component/product-card/product-card.interface";
 import ProductContainer from "../../container/product-container/product-container";
+import { getShopProductsCollectionAndDocument } from "../../services/firestore-config";
 import PageLayout from "./../../layout/public-page";
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useState, useEffect, useTransition } from "react";
+import toast, { Toaster } from "react-hot-toast";
 const ProductCartSkeleton = lazy(
   () => import("../../container/skeleton/product-cart")
 );
 
 export default function Shop() {
-  const productList = [
-    {
-      id: 1,
-      productTitle: "قهوه",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 59900,
-      productCombination: {
-        item1: "نوع A",
-        item2: "نوع B",
-      },
-    },
-    {
-      id: 2,
-      productTitle: "کاپوچینو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 60000,
-      productCombination: {
-        item1: "شیرین",
-        item2: "تلخ",
-      },
-    },
-    {
-      id: 3,
-      productTitle: "لاته",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 72000,
-      productCombination: {
-        item1: "بزرگ",
-        item2: "کوچک",
-      },
-    },
-    {
-      id: 4,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 5,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 6,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 7,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 8,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 9,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 10,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 11,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 12,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 11,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 12,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 11,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-    {
-      id: 12,
-      productTitle: "اسپرسو",
-      productImage: "/assetes/image/home-assetes/2.jpg",
-      productPrice: 420010,
-      productCombination: {
-        item1: "قوی",
-        item2: "ملایم",
-      },
-    },
-  ];
+  const [productList, setProductList] = useState<ProductDetails[]>();
+  const [isPending, startTransition] = useTransition();
+  console.log(isPending);
+  useEffect(() => {
+    getShopProductsCollectionAndDocument("products")
+      .then((response) => {
+        startTransition(() => {
+          setProductList(response);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error("لطفا فیلتر شکن خود را متصل کنید");
+      });
+  }, []);
 
   return (
     <PageLayout>
       <section className="px-2 pb-10">
-        <Suspense fallback={<ProductCartSkeleton />}>
+        {!productList ? (
+          <ProductCartSkeleton />
+        ) : (
           <ProductContainer productList={productList} title={""} />
-        </Suspense>
+        )}
+        <Toaster />
       </section>
     </PageLayout>
   );
