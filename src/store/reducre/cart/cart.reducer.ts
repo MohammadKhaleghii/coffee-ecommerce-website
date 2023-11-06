4c3d2f7297cb9fdeb4689ccdda151eb891d38690
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-06 11:44:24 +0330

    add cart reducer

--- src/store/reducre/cart/cart.reducer.ts
+++ src/store/reducre/cart/cart.reducer.ts
@@ -1,12 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { CartItemProps } from "../../../component/cart-item/cart-item.interface";
 
-const initialState: any = [];
+const initialState: CartItemProps[] = [];
 
 const cartSlice = createSlice({
   name: "cart",
-  reducers: {},
   initialState,
+  reducers: {
+    addToCart: (state, action) => {},
+    decreaseItemInTocart: (state, action) => {},
+    removeFromCart: (state, action) => {},
+  },
 });
 
-
-export default cartSlice
+export default cartSlice.reducer;
