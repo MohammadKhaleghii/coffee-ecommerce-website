f6f7edb2ab068da705ae01193c30eae123760490
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-06 14:19:40 +0330

    add cart actions

--- src/component/cart-item/cart-item.tsx
+++ src/component/cart-item/cart-item.tsx
@@ -1,5 +1,11 @@
+import {
+  addToCart,
+  decreaseItemInTocart,
+  removeFromCart,
+} from "../../store/reducre/cart/cart.reducer";
 import { thousandSeparator } from "../../utilities/thousand-separator";
 import { CartItemProps } from "./cart-item.interface";
+import { useDispatch } from "react-redux";
 
 const CartItem: React.FC<CartItemProps> = ({
   imagePath,
@@ -9,6 +15,25 @@ const CartItem: React.FC<CartItemProps> = ({
   productTitle,
   productQTY,
 }: CartItemProps) => {
+  const dispatch = useDispatch();
+  const product = {
+    imagePath,
+    productBrand,
+    productID,
+    productPrice,
+    productTitle,
+    productQTY,
+  };
+  const handleDeleteproduct = () => {
+    dispatch(removeFromCart(product));
+  };
+
+  const handleAddProduct = () => {
+    dispatch(addToCart(product));
+  };
+  const handleDecreaseProductQTY = () => {
+    dispatch(decreaseItemInTocart(product));
+  };
   return (
     <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
       <div className="flex w-2/5">
@@ -18,16 +43,16 @@ const CartItem: React.FC<CartItemProps> = ({
         <div className="flex flex-col justify-between ml-4 flex-grow">
           <span className="font-bold text-sm">{productTitle}</span>
           <span className="text-red-500 text-xs">{productBrand}</span>
-          <a
-            href="#"
-            className="font-semibold hover:text-red-500 text-gray-500 text-xs"
+          <button
+            onClick={handleDeleteproduct}
+            className="font-semibold hover:text-red-500 text-gray-500 text-xs text-right"
           >
             حذف از سبد
-          </a>
+          </button>
         </div>
       </div>
       <div className="flex justify-center items-center w-1/5">
-        <i className="fa-solid fa-plus text-sm"></i>
+        <i onClick={handleAddProduct} className="fa-solid fa-plus text-sm"></i>
 
         <input
           className="mx-2 border text-center w-8"
@@ -35,7 +60,10 @@ const CartItem: React.FC<CartItemProps> = ({
           value={productQTY.toString()}
         />
 
-        <i className="fa-solid fa-minus text-sm "></i>
+        <i
+          onClick={handleDecreaseProductQTY}
+          className="fa-solid fa-minus text-sm "
+        ></i>
       </div>
       <span className="text-center w-1/5 font-semibold text-sm">
         {thousandSeparator(productPrice)} تومان
