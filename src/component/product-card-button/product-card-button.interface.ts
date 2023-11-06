c72569584f1ddbb1a5f085031722721d74988ae0
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-06 11:56:41 +0330

    format product card button CM

--- src/component/product-card-button/product-card-button.interface.ts
+++ src/component/product-card-button/product-card-button.interface.ts
@@ -1,1 +1,16 @@
-export interface ProductCardButtonProps{}
+export interface ProductCardButtonProps {
+  product: ProductDetails;
+}
+
+interface ProductDetails {
+  imagePath: string;
+  productTitle: string;
+  productBrand: string;
+  productID: number;
+  productPrice: number;
+  productCombination: ProductCombination;
+}
+interface ProductCombination {
+  item1: string;
+  item2: string;
+}
--- src/component/product-card-button/product-card-button.tsx
+++ src/component/product-card-button/product-card-button.tsx
@@ -1,12 +1,23 @@
+import { addToCart } from "../../store/reducre/cart/cart.reducer";
 import { ProductCardButtonProps } from "./product-card-button.interface";
+import { useDispatch } from "react-redux";
 
-const ProductCartButton: React.FC<ProductCardButtonProps> = (props: ProductCardButtonProps)=>{
-    return (
-        <div className="flex flex-row gap-x-2 justify-center items-center pb-2">
-          <button className="rounded-full bg-[#F9C06A] px-4 py-2 w-fit border-none">
-            افزودن به سبد خرید
-          </button>
-        </div>
-    );
+const ProductCartButton: React.FC<ProductCardButtonProps> = (
+  props: ProductCardButtonProps
+) => {
+  const dispatch = useDispatch();
+  const handleAddItemToCart = () => {
+    dispatch(addToCart(props.product));
+  };
+  return (
+    <div className="flex flex-row gap-x-2 justify-center items-center pb-2">
+      <button
+        onClick={handleAddItemToCart}
+        className="rounded-full bg-[#F9C06A] px-4 py-2 w-fit border-none"
+      >
+        افزودن به سبد خرید
+      </button>
+    </div>
+  );
 };
 export default ProductCartButton;
