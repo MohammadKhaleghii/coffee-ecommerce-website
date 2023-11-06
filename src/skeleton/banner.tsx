e49095b9f1834d82fcc68073d20876feaa5fb1a0
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-06 11:27:06 +0330

    add banner skeleton

---
+++ src/skeleton/banner.tsx
@@ -0,0 +1,7 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const BannerSkeleton = () => {
+  return <Skeleton className="rounded-2xl lg:h-[200px] h-[60px]" />;
+};
+export default BannerSkeleton;
