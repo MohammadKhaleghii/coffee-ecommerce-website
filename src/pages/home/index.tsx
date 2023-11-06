504bae2d8768ca82e066d7d13e5ffa1301e8009d
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-06 14:20:18 +0330

    add link to banner

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,7 +5,6 @@ import SliderSkeleton from "../../skeleton/slider";
 import Slider from "../../container/slider/slider";
 import { SliderInput } from "../../services/dto/slider-input";
 import {
-  addCollecionAndDocment,
   getCategoriesCollectionAndDocument,
   getSlidersCollectionAndDocument,
   getSpecialProductsCollectionAndDocument,
@@ -16,6 +15,7 @@ import { lazy, useEffect, useState, Suspense } from "react";
 import CategoryItemSkeleton from "../../skeleton/category-item";
 import { CategoryItemInput } from "../../services/dto/category-item-input";
 import BannerSkeleton from "../../skeleton/banner";
+import { Link } from "react-router-dom";
 
 const ProductContainer = lazy(
   () => import("../../container/product-container/product-container")
@@ -75,13 +75,14 @@ export default function Home() {
         )}
       </section>
       <section className="pt-10 pb-10 w-full h-auto lg:px-[130px] px-5">
-        
         <Suspense fallback={<BannerSkeleton />}>
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/coffe-web-bc1c8.appspot.com/o/1110.png?alt=media&token=0cdfe04b-265f-401d-b7a6-98af8bfa6a17&_gl=1*czn4uw*_ga*MTU2NDYwNTAzLjE2OTY4NjUwMzY.*_ga_CW55HF8NVT*MTY5OTI1Mzg2MS4yMC4xLjE2OTkyNTcwNjMuNTYuMC4w"
-            className="w-full h-auto"
-            alt=""
-          />
+          <Link to={"/shop"}>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/coffe-web-bc1c8.appspot.com/o/1110.png?alt=media&token=0cdfe04b-265f-401d-b7a6-98af8bfa6a17&_gl=1*czn4uw*_ga*MTU2NDYwNTAzLjE2OTY4NjUwMzY.*_ga_CW55HF8NVT*MTY5OTI1Mzg2MS4yMC4xLjE2OTkyNTcwNjMuNTYuMC4w"
+              className="w-full h-auto"
+              alt=""
+            />
+          </Link>
         </Suspense>
       </section>
       <section className=" pb-10">
