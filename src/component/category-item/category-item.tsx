5a6309b640c78d9f4e80019a92a2189a4343aa7f
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2023-11-11 19:42:37 +0330

    add hover for category item

--- src/component/category-item/category-item.tsx
+++ src/component/category-item/category-item.tsx
@@ -4,13 +4,13 @@ const CategoryItem: React.FC<CategoryItemProps> = (
   props: CategoryItemProps
 ) => {
   return (
-    <div className="bg-gradient-to-r from-[#54372B] to-[#25181A] rounded-2xl flex flex-col items-center justify-center gap-y-3 py-10 ">
+    <div className="bg-gradient-to-r from-[#54372B] to-[#25181A] hover:bg-gradient-to-r hover:duration-100	  hover:from-[#664334] hover:to-[#25181A]  rounded-2xl flex flex-col items-center justify-center gap-y-3 py-10 hover:-translate-y-2">
       <div className="w-[107px] h-[107px] flex items-center justify-center">
         <img
           loading="lazy"
           src={props.categoryImage}
           alt=""
-          className="min-w-max"
+          className="min-w-max hover:scale-110 duration-100	"
         />
       </div>
       <h3 className="text-primary-20 font-bold ">{props.categoryTitle}</h3>
