cb1bb3d649223d2524712228dd65fa9b68a73b68
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2023-11-11 19:46:09 +0330

    update product cart tailwind class

--- src/component/product-card-button/product-card-button.tsx
+++ src/component/product-card-button/product-card-button.tsx
@@ -14,7 +14,7 @@ const ProductCartButton: React.FC<ProductCardButtonProps> = (
     <div className="flex flex-row gap-x-2 justify-center items-center pb-2">
       <button
         onClick={handleAddItemToCart}
-        className="rounded-full bg-[#F9C06A] px-4 py-2 w-fit border-none"
+        className="primary-button"
       >
         افزودن به سبد خرید
       </button>
