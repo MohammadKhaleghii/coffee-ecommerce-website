28e1bdc6cae9e6790056779df6fa207fe6b2ba3e
Mohammad Khaleghi <77498259+MohammadKhaleghii@users.noreply.github.com>
2023-11-14 12:58:14 +0330

    add lazy loading to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { Route, Routes } from "react-router-dom";
-import Home from './pages/home/index';
-import Shop from "./pages/shop";
-import Suppout from "./pages/suppout";
-import Contact from "./pages/contact";
-import About from "./pages/about";
-import Cart from "./pages/cart";
-import User from "./pages/user";
+import { Suspense, lazy } from "react";
+import Spinner from "./component/spinner/spinner";
+const Home = lazy(()=> import( "./pages/home/index")  )
+const Shop = lazy(() => import("./pages/shop"));
+const Suppout = lazy(() => import("./pages/suppout"));
+const Contact = lazy(() => import("./pages/contact"));
+const About = lazy(() => import("./pages/about"));
+const Cart = lazy(() => import("./pages/cart"));
+const User = lazy(() => import("./pages/user"));
 
 function App() {
   return (
-    <Routes>
-      <Route Component={Home} path="/" index />
-      <Route Component={Shop} path="/shop" />
-      <Route Component={Suppout} path="/suppout" />
-      <Route Component={Contact} path="/contact" />
-      <Route Component={About} path="/about" />
-      <Route Component={Cart} path="/cart" />
-      <Route Component={User} path="/user" />
-    </Routes>
+    <Suspense>
+      <Routes>
+        <Route Component={Home} path="/" index />
+        <Route Component={Shop} path="/shop" />
+        <Route Component={Suppout} path="/suppout" />
+        <Route Component={Contact} path="/contact" />
+        <Route Component={About} path="/about" />
+        <Route Component={Cart} path="/cart" />
+        <Route Component={User} path="/user" />
+      </Routes>
+    </Suspense>
   );
 }
 
