72d0ed18f2bbf881a86bc94f59c84622b4d13d29
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2023-12-04 14:41:14 +0330

    fix app issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import { Route, Routes } from "react-router-dom";
-import { Suspense, lazy } from "react";
-import Spinner from "./component/spinner/spinner";
-const Home = lazy(()=> import( "./pages/home/index")  )
-const Shop = lazy(() => import("./pages/shop"));
-const Suppout = lazy(() => import("./pages/suppout"));
-const Contact = lazy(() => import("./pages/contact"));
-const About = lazy(() => import("./pages/about"));
-const Cart = lazy(() => import("./pages/cart"));
-const User = lazy(() => import("./pages/user"));
-
-function App() {
-  return (
-    <Suspense>
-      <Routes>
-        <Route Component={Home} path="/" index />
-        <Route Component={Shop} path="/shop" />
-        <Route Component={Suppout} path="/suppout" />
-        <Route Component={Contact} path="/contact" />
-        <Route Component={About} path="/about" />
-        <Route Component={Cart} path="/cart" />
-        <Route Component={User} path="/user" />
-      </Routes>
-    </Suspense>
-=======
 import {Route, Routes} from "react-router-dom";
 import Home from "./routes/home/index";
 import support from "./routes/support";
@@ -44,7 +18,6 @@ function App() {
       <Route Component={Cart} path="/cart" />
       <Route Component={User} path="/user" />
     </Routes>
->>>>>>> 63aee3a (update folder names)
   );
 }
 
