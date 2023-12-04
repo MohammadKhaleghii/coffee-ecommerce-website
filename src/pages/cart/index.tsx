083544e797296283c90723bab32b052a8893bd75
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2023-12-04 09:54:19 +0330

    remove unused heading

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,8 +1,8 @@
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import CartItem from "../../component/cart-item/cart-item";
 import PageLayout from "./../../layout/public-page";
-import { CartItemProps } from "../../component/cart-item/cart-item.interface";
-import { Toaster } from 'react-hot-toast';
+import {CartItemProps} from "../../component/cart-item/cart-item.interface";
+import {Toaster} from "react-hot-toast";
 
 export default function Cart() {
   const selectCart: CartItemProps[] = useSelector((state: any) => state.cart);
@@ -11,43 +11,40 @@ export default function Cart() {
     <PageLayout>
       <section className="px-2 pb-10">
         {selectCart.length ? (
-          <>
-            <div className="container mx-auto mt-10">
-              <div className="flex shadow-md my-10">
-                <div className="w-full bg-white px-10 py-10">
-                  <div className="flex justify-between border-b pb-8">
-                    <h1 className="font-semibold text-2xl">سبد خرید</h1>
-                    <h2 className="font-semibold text-2xl"></h2>
-                  </div>
-                  <div className="flex mt-10 mb-5">
-                    <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">
-                      جزئیات محصول
-                    </h3>
-                    <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
-                      تعداد
-                    </h3>
-                    <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
-                      قیمت
-                    </h3>
-                    <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
-                      جمع
-                    </h3>
-                  </div>
-                  {selectCart.map((item) => (
-                    <CartItem
-                      key={item.productID}
-                      imagePath={item.imagePath}
-                      productBrand={item.productBrand}
-                      productID={item.productID}
-                      productPrice={item.productPrice}
-                      productTitle={item.productTitle}
-                      productQTY={item.productQTY}
-                    />
-                  ))}
+          <div className="container mx-auto mt-10">
+            <div className="flex shadow-md my-10">
+              <div className="w-full bg-white px-10 py-10">
+                <div className="flex justify-between border-b pb-8">
+                  <h1 className="font-semibold text-2xl">سبد خرید</h1>
                 </div>
+                <div className="flex mt-10 mb-5">
+                  <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">
+                    جزئیات محصول
+                  </h3>
+                  <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
+                    تعداد
+                  </h3>
+                  <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
+                    قیمت
+                  </h3>
+                  <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
+                    جمع
+                  </h3>
+                </div>
+                {selectCart.map((item) => (
+                  <CartItem
+                    key={item.productID}
+                    imagePath={item.imagePath}
+                    productBrand={item.productBrand}
+                    productID={item.productID}
+                    productPrice={item.productPrice}
+                    productTitle={item.productTitle}
+                    productQTY={item.productQTY}
+                  />
+                ))}
               </div>
             </div>
-          </>
+          </div>
         ) : (
           <div className="flex items-center justify-center flex-col">
             <img
